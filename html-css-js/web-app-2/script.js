83fe07aaf2f52df259ab626f38870e932079bd64
Venkatreddy71 <morthalavenkatreddy123@gmail.com>
2026-01-12 16:14:34 +0530

    new changes

---
+++ html-css-js/web-app-2/script.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    name: "Product 1",
+    desc: "This is a sample description",
+    price: 45,
+    imgUrl: "https://picsum.photos/id/1/300/300",
+    cart: "Add to cart",
+  },
+  {
+    name: "Product 2",
+    desc: "This is a sample description",
+    price: 50,
+    imgUrl: "https://picsum.photos/id/2/300/300",
+    cart: "Add to cart",
+  },
+  {
+    name: "Product 3",
+    desc: "This is a sample description",
+    price: 70,
+    imgUrl: "https://picsum.photos/id/3/300/300",
+    cart: "Add to cart",
+  },
+];
+
+function showProducts() {
+  let str = `<div class="row">`;
+  products.map(e=>{
+    str = str + `
+    <div class="box">
+    <img src=${e.imgUrl}/>
+    <h3>${e.name}</h3>
+    <p>${e.desc}</p>
+    <h5>${e.price}</h5>
+    <button>${e.cart}</button>
+    </div>
+      `
+  })
+  str = str + `</div>`;
+  root.innerHTML = str;
+}
