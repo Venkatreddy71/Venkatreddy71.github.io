8ca3211365da651e840bb573b46d5211eec80ad1
Venkatreddy71 <morthalavenkatreddy123@gmail.com>
2026-03-14 11:23:41 +0530

    new changes

--- node-app/index.js
+++ node-app/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+import express from 'express'
+const app=express()
+const startServer=()=>{
+    app.listen(5000,()=>{
+        console.log("Server Started")
+    })
+}
+app.use('/admin/products',productRouter)
+startServer()
